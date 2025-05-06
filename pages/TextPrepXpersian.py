c3efe4ff258cdf71f558d420921585e4deed582d
Farhad <88395627+farhad-here@users.noreply.github.com>
2025-05-06 23:43:09 +0330

    Update TextPrepXpersian.py
    fix bug 1.9

--- pages/TextPrepXpersian.py
+++ pages/TextPrepXpersian.py
@@ -171,7 +171,7 @@ html_code = f"""
               font-size: 24px;
               line-height: 1.8;
               ">
-              {oop.finalCleaning()}
+              {PersianText(text).finalCleaning()}
               </div>
               """
 st.markdown(html_code, unsafe_allow_html=True)
